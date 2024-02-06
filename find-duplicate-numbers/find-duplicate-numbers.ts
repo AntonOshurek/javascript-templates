da850c3f23b55eb443df0ec2aa1faaef2b98b70a
Anton Ashurek <antonashurek@192.168.1.1>
2024-02-06 14:05:35 +0100

    add find duplicate function

--- find-duplicate-numbers/find-duplicate-numbers.ts
+++ find-duplicate-numbers/find-duplicate-numbers.ts
@@ -1,8 +1,8 @@
-function findDuplicates(arr: number[]) {
+const findDuplicates = (arr: number[]): number[] => {
   return arr.filter((value, index, self) => {
     return self.indexOf(value) !== index;
   });
-}
+};
 
 let myArray = [1, 2, 3, 4, 2, 5, 6, 3, 7, 8, 9];
 let duplicateValues = findDuplicates(myArray);
