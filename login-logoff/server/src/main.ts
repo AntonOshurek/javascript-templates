8e7eb5ed08fc5e69239f3db8681a5055676390dc
Anton Oshurek <oshurekdev@proton.me>
2024-11-27 21:57:56 +0100

    add logger

--- login-logoff/server/src/main.ts
+++ login-logoff/server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app/app.module';
+import { LoggerMiddleware } from './middleware/logger.midleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(helmet());
   app.use(cookieParser());
+  app.use(new LoggerMiddleware().use);
 
   await app.listen(process.env.PORT ?? 3000);
 }
